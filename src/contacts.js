0e5b101233caddf9d07552c396c875ef29078231
DJKwan1228 <163971607+DJKwan1228@users.noreply.github.com>
2024-05-28 01:05:33 +0800

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const contacts = [
+  {
+    name: "Garnacho",
+    imgURL:
+      "https://www.thesun.co.uk/wp-content/uploads/2023/09/alejandro-garnacho-manchester-united-premier-841683391.jpg?strip=all&quality=100&w=1080&h=1080&crop=1",
+    position: "Winger",
+    email: "Garna11@devil.com",
+  },
+  {
+    name: "Rashford",
+    imgURL:
+      "https://static.independent.co.uk/2024/04/26/09/newFile-1.jpg?width=1200&height=1200&fit=crop",
+    position: "Striker",
+    email: "Ras10@devil.com",
+  },
+  {
+    name: "Dalot",
+    imgURL:
+      "https://img.a.transfermarkt.technology/portrait/big/357147-1602074624.jpg?lm=1",
+    position: "Right Back",
+    email: "dalot@devil.com",
+  },
+];
+
+export default contacts;
